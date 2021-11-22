f37500e41e087d9ac75b5a7cc77775b8afe86837
mlkabdazz <themalekmalek@gmail.com>
2021-11-23 01:09:08 +0700

    add source for UI the apps

---
+++ app/src/main/java/com/mlkabdazz/pickfish/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.mlkabdazz.pickfish;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // remove action bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }, 3000L);
+    }
+}
