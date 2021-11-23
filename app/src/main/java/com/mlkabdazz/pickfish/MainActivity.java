8b6e485463eb4f5329762b21b34c0831976fd508
mlkabdazz <themalekmalek@gmail.com>
2021-11-24 01:57:50 +0700

    change ui and remove icon

--- app/src/main/java/com/mlkabdazz/pickfish/MainActivity.java
+++ app/src/main/java/com/mlkabdazz/pickfish/MainActivity.java
@@ -1,98 +1,52 @@
 package com.mlkabdazz.pickfish;
 
 import android.graphics.Color;
-import android.os.Handler;
-import android.os.Looper;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.util.Log;
 
-import com.wooplr.spotlight.SpotlightView;
-import com.wooplr.spotlight.prefs.PreferencesManager;
+import github.com.st235.lib_expandablebottombar.ExpandableBottomBar;
+import github.com.st235.lib_expandablebottombar.Menu;
+import github.com.st235.lib_expandablebottombar.MenuItemDescriptor;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private static final String IB_GENERAL_BUTTON = "ib_general_model";
-    private static final String IB_SPECIFIC_BUTTON = "ib_specific_model";
-    private static final String IB_GALLERY = "ib_gallery";
-    private static final String IB_INFORMATION = "ib_information";
-    private SpotlightView spotlight;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
-    ImageButton ibGeneralButton;
-    ImageButton ibSpecificModel;
-    ImageButton ibGallery;
-    ImageButton ibInformation;
+    ExpandableBottomBar expandableBottomBar;
+    Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ibGeneralButton = findViewById(R.id.ib_general_model);
-        ibSpecificModel = findViewById(R.id.ib_specific_model);
-        ibGallery = findViewById(R.id.ib_gallery);
-        ibInformation = findViewById(R.id.ib_information);
+        expandableBottomBar = findViewById(R.id.expandable_bottom_bar);
+        menu = expandableBottomBar.getMenu();
 
-        ibInformation.setOnClickListener(this);
-        ibSpecificModel.setOnClickListener(this);
-        ibGeneralButton.setOnClickListener(this);
-        ibGallery.setOnClickListener(this);
+        menu.add(
+                new MenuItemDescriptor.Builder(this, R.id.icon_fish_gray, R.drawable.ic_fish_gray, R.string.realtime, Color.GRAY).build()
+        );
+        menu.add(
+                new MenuItemDescriptor.Builder(this, R.id.icon_gallery, R.drawable.ic_gallery, R.string.gallery, Color.GRAY).build()
+        );
+
+        menu.add(
+                new MenuItemDescriptor.Builder(this, R.id.icon_information, R.drawable.ic_information, R.string.information, Color.GRAY).build()
+        );
+
+
+        expandableBottomBar.setOnItemReselectedListener((view, item, byUser) -> {
+            Log.d(TAG, "selected: " + item.toString());
+            return null;
+        });
+
+        expandableBottomBar.setOnItemReselectedListener((view, item, byUser) -> {
+            Log.d(TAG, "reselected: " + item.toString());
+            return null;
+        });
+
     }
-
-    @Override
-    public void onClick(View v) {
-        PreferencesManager preferencesManager = new PreferencesManager(MainActivity.this);
-        switch (v.getId()) {
-            case R.id.ib_information:
-                preferencesManager.resetAll();
-                new Handler(Looper.getMainLooper()).postDelayed(() -> {
-                    showIntro(ibInformation, IB_INFORMATION, "Information", "UX not ready yet!");
-                }, 400);
-                break;
-            case R.id.ib_general_model:
-                preferencesManager.resetAll();
-                new Handler(Looper.getMainLooper()).postDelayed(() -> {
-                    showIntro(ibGeneralButton, IB_GENERAL_BUTTON, "Coming soon!", "A deep learning model for all or general fish.");
-                }, 400);
-                break;
-            case R.id.ib_specific_model:
-                preferencesManager.resetAll();
-                new Handler(Looper.getMainLooper()).postDelayed(() -> {
-                    showIntro(ibSpecificModel, IB_SPECIFIC_BUTTON, "Coming soon!", "A deep learning model for specific fish");
-                }, 400);
-                break;
-            case R.id.ib_gallery:
-                preferencesManager.resetAll();
-                new Handler(Looper.getMainLooper()).postDelayed(() -> {
-                    showIntro(ibGallery, IB_GALLERY, "Coming soon!", "Select pict of fish from gallery");
-                }, 400);
-                break;
-        }
-    }
-
-    private void showIntro(View view, String usageId, String heading, String subHeading) {
-        new SpotlightView.Builder(this)
-                .introAnimationDuration(100)
-                .enableRevealAnimation(true)
-                .performClick(true)
-                .fadeinTextDuration(400)
-                .headingTvColor(Color.parseColor("#eb273f"))
-                .headingTvSize(32)
-                .headingTvText(heading)
-                .subHeadingTvColor(Color.parseColor("#ffffff"))
-                .subHeadingTvSize(16)
-                .subHeadingTvText(subHeading)
-                .maskColor(Color.parseColor("#dc000000"))
-                .target(view)
-                .lineAnimDuration(400)
-                .lineAndArcColor(Color.parseColor("#eb273f"))
-                .dismissOnTouch(true)
-                .dismissOnBackPress(true)
-                .enableDismissAfterShown(true)
-                .usageId(usageId)
-                .show();
-    }
-
 }
